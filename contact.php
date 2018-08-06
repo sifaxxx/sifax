ab951e78fbf01e15fdf4ff315f69918bb1c40ab0
siphax <syphax_ka@hotmail.com>
2018-08-06 11:59:52 +0200

    ajouter un lien dans contact fix #2

--- contact.php
+++ contact.php
@@ -1,3 +1,9 @@
 <h1>contact</h1>
 <p>paragraphe sur master</p>
-<p>new operation</p>
+
+<p>google</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde commodi aperiam, doloribus earum assumenda, esse laudantium repellendus id eveniet rerum quibusdam voluptatum! Expedita, vitae, placeat! Distinctio commodi, dicta ipsum numquam!</p>
+
+<a href="#">lien fix bug</a>
+
